07f712ac522f17410a22bb0690fddbaaf27d5ea9
ducksfrogs <ma_yamaki@yahoo.com>
2023-06-13 10:55:25 +0900

    sublime merge test

---
+++ next_lesson/testEnv/index.js
@@ -0,0 +1,9 @@
+import {useState } from 'react';
+
+function Header({ title }){
+	return <h1>{ title ? title: "default title" }</h1>;
+}
+
+function HomePage() {
+	const names = ['Ada Lovelace', 'Grace Hopper']
+}
